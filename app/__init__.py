e5dbfd7c2519000ddd2015348d9f683016128aa0
Florian Frosch <22937059+ffrosch@users.noreply.github.com>
2022-12-01 01:02:57 +0100

    feat: add basic blueprint structure and content

--- app/__init__.py
+++ app/__init__.py
@@ -18,6 +18,18 @@ def create_app(config_class=Config):
 
     db.init_app(app)
 
+    from app.errors import bp as errors_bp
+
+    app.register_blueprint(errors_bp)
+
+    from app.main import bp as main_bp
+
+    app.register_blueprint(main_bp)
+
+    from app.api import bp as api_bp
+
+    app.register_blueprint(api_bp, url_prefix="/api")
+
     # The models are needed to initialize the db
     from . import models  # noqa
 
